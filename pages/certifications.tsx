025a48b1de60f2e06b5d9463c7769ae795fae67d
Alonso Lacayo <lacayo004@gmail.com>
2025-06-09 21:32:55 -0500

    add default exports for contact and certification

---
+++ pages/certifications.tsx
@@ -0,0 +1,8 @@
+export default function Certifications() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Certifications Page</h1>
+      </div>
+    );
+  }
+  
