b3b2a69a1b81abb6fd860b39d5139683b5a4b062
Alonso Lacayo <lacayo004@gmail.com>
2025-06-11 16:50:04 -0500

    Update other files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,7 @@
 export default function Footer() {
-    return (
-      <footer className="mt-16 text-center text-sm text-gray-500 py-6">
-        © {new Date().getFullYear()} Alonso Lacayo. All rights reserved.
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="text-center text-sm text-white/60 py-4">
+      © {new Date().getFullYear()} Alonso Lacayo. All rights reserved.
+    </footer>
+  );
+}
